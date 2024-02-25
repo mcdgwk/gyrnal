2238e46a95b858d2f47bd0a7cc59f0fb19992c3b
megan <meganmcdougall71@gmail.com>
2024-02-25 00:19:36 +0000

    updates to dashboard photos - more plans added

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -49,14 +49,49 @@ class DashboardScreen extends StatelessWidget {
 
         ],
         exerciseNames: [
-          'Bicep Barbell Curl',
+          'Barbell Bicep Curl',
           'Dumbbell Bicep Curl',
           'Chest Supported Dumbbell Row',
           'Dumbbell Shrug',
           'Standing Cable Rear Delt Fly',
         ],
       ),
-      // todo - add more plans - 1 more for the dashboard??
+
+      WorkoutPlan(
+        planName: 'Push Workout Plan',
+        imageUrls: [
+          'assets/barbell-bench-press.png', // ref - https://strengthbuzz.com/fitness/how-to-do-barbell-bench-press-chest-workout.html
+          'assets/incline-dumbbell-press.jpeg', // ref - http://musclemediaonline.com/3-exercises-you-should-do-for-a-stronger-bench/dumbbell-bench-press-while-lying-on-an-incline-bench-exercising-for-bodybuilding-target-muscles-are-marked-in-red-initial-and-final-steps-3d-illustration/
+          'assets/barbell-shoulder-press.png', // ref - https://animalia-life.club/qa/pictures/barbell-shoulder-press
+          'assets/leaning-cable-lateral-raise.jpg', // ref - https://fitliferegime.com/leaning-lateral-raise-muscle-worked-benefit-and-tips/
+          'assets/tricep-pushdown.jpg', // ref - https://strengthlevel.com/strength-standards/tricep-pushdown/kg
+        ],
+        exerciseNames: [
+          'Barbell Bench Press',
+          'Incline Dumbbell Press',
+          'Barbell Shoulder Press',
+          'Leaning Cable Lateral Raises',
+          'Triceps Pushdown',
+        ],
+      ),
+
+      WorkoutPlan(
+        planName: 'Legs Workout Plan',
+        imageUrls: [
+          'assets/back-squat.png', // ref - https://fitnessvolt.com/front-squat-vs-back-squat/
+          'assets/barbell-romanian-deadlift.jpg', // ref - https://www.inspireusafoundation.org/romanian-deadlift/
+          'assets/leg-extension.jpg', // ref - https://www.gofitnessplan.com/exercises/seated-leg-extensions
+          'assets/lying-hamstring-curls.jpeg', // ref - https://www.jasestuart.com/lying-hamstring-curls
+          'assets/bodyweight-calves-raises.jpg', // ref - https://www.gofitnessplan.com/exercises/calves-raises
+        ],
+        exerciseNames: [
+          'Back Squat',
+          'Barbell Romanian Deadlift',
+          'Leg Extensions',
+          'Hamstring Curls',
+          'Bodyweight Calf Raises',
+        ],
+      ),
     ];
 
 
@@ -68,47 +103,58 @@ class DashboardScreen extends StatelessWidget {
         title: 'Gyrnal',
       ),
       body: Container(
-        child: Column(
-          children: [
-        Text(
-        'Hello user!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey,
-          fontFamily: 'Montserrat',
-        ),
-        ),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  "Coming soon to Gyrnal!",
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    fontFamily: 'Montserrat',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+          Text(
+          'Hello user!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'Montserrat',
+          ),
+          ),
+              Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "Coming soon to Gyrnal!",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'Montserrat',
+                    ),
                   ),
-                ),
-            ),
-            // Sliding Carousel to display users plans
-            // Todo - Change to workouts instead of text imgs
-            // on click -> go to workout
-            // current items = test photos
+              ),
+              // Sliding Carousel to display users plans
+              // Todo - Change to workouts instead of text imgs
+              // on click -> go to workout
+              // current items = test photos
+          
+          
+              // Reference - https://www.youtube.com/watch?v=p3gFikowJVI
+          
+              // test carousel
+              // CarouselWithDots(testImagesList: testImagesList),
+              CarouselWithDots(workoutPlan: preMadeWorkouts[0]),
+              SizedBox(
+                height: 30,
+              ),
+              CarouselWithDots(workoutPlan: preMadeWorkouts[1]),
+              SizedBox(
+                height: 30,
+              ),
 
 
-            // Reference - https://www.youtube.com/watch?v=p3gFikowJVI
-
-            // test carousel
-            // CarouselWithDots(testImagesList: testImagesList),
-            CarouselWithDots(workoutPlan: preMadeWorkouts[0]),
-            SizedBox(
-              height: 30,
-            ),
-            // CarouselWithDots(testImagesList: testImagesList),
-          ]
-      ),
+              CarouselWithDots(workoutPlan: preMadeWorkouts[2]),
+              SizedBox(
+                height: 30,
+              ), // CarouselWithDots(testImagesList: testImagesList),
+            ]
+          ),
+        ),
       ),
     );
   }
