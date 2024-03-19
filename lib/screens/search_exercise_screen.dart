761b8c96257c9c21fe64d80aa8dc149e5541cda6
megan <meganmcdougall71@gmail.com>
2024-03-19 22:27:54 +0000

    search exercise screen

---
+++ lib/screens/search_exercise_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:gyrnal_workout_app/models/create_exercise_model.dart';
+import 'package:gyrnal_workout_app/widgets/exercise_widget.dart';
+
+// ignore: must_be_immutable
+
+// allows users to search for exercises
+class SearchExerciseScreen extends StatefulWidget {
+  // list of all exercises
+  final List<CreateExerciseModel> exercises;
+  // filtered list of exercises based on search query
+  List<CreateExerciseModel> filteredExercises = [];
+
+
+  SearchExerciseScreen({super.key, required this.exercises}) {
+    filteredExercises = exercises;
+  }
+
+  @override
+  State<SearchExerciseScreen> createState() => _SearchExerciseScreenState();
+}
+
+class _SearchExerciseScreenState extends State<SearchExerciseScreen> {
+  // filters search based on user input
+  void filterExercises(value) {
+    setState(() {
+      widget.filteredExercises = widget.exercises
+          .where((exercise) =>
+          // all input to lower case so case sensitivity doesn't matter
+          exercise.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          // calls function each time input is changed
+          onChanged: (value) {
+            filterExercises(value);
+          },
+          decoration: const InputDecoration(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              hintText: "Search Exercise",
+              hintStyle: TextStyle(color: Colors.white)),
+        ),
+        actions: <Widget>[
+          // allows user to cancel search and return to previous screen
+          IconButton(
+            icon: const Icon(Icons.cancel),
+            onPressed: () {
+              setState(() {
+                Navigator.pop(context);
+              });
+            },
+          )
+        ],
+      ),
+
+      // builds list of exercises if they match the search query
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: widget.filteredExercises.isNotEmpty
+            ? ListView.builder(
+            itemCount: widget.filteredExercises.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ExerciseWidget(widget.filteredExercises[index]);
+            })
+            // displays if no matches
+            : const Center(
+          child: Text('Exercise not found...'),
+        ),
+      ),
+    );
+  }
+}
