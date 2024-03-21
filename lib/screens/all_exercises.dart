4609b7b6f106414809ec7533284eefd5e806549e
megan <meganmcdougall71@gmail.com>
2024-03-21 13:30:01 +0000

    all exercises implemented

---
+++ lib/screens/all_exercises.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:gyrnal_workout_app/services/exercise_provider.dart';
+import 'package:gyrnal_workout_app/widgets/exercise_widget.dart';
+
+// lists all exercises
+class AllExercisesScreen extends StatelessWidget {
+  const AllExercisesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // consumer class listen for changes -> any updates to exercise list
+    return Consumer<ExerciseClass>(
+      builder: (BuildContext context, provider, Widget? child) {
+        // listview builds exercise list
+        return ListView.builder(
+            itemCount: provider.allExercises.length,
+            itemBuilder: (context, index) {
+              // for each exercise, the ExerciseWidget displays the exercise information
+              return ExerciseWidget(provider.allExercises[index]);
+            });
+      },
+    );
+  }
+}
